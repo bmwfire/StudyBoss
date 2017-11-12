f83c18b4682c75ab8ad1138e071550d3665bff8f
Brandon Wong <bwong7@hawaii.edu>
2017-11-11 18:59:49 -1000

    test_2

--- StudyBoss/SecondViewController.swift
+++ StudyBoss/SecondViewController.swift
@@ -9,14 +9,12 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-    
-    //Brandon Comment
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    //Andrew's Comment
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
