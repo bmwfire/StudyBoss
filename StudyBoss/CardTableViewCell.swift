415a3be459c0a90e92afe52b6f860f45573b583f
Andrew Millard <AKM@Andrews-MacBook-Pro.local>
2017-12-05 02:22:21 -1000

    new implementation of adding cards

--- StudyBoss/CardTableViewCell.swift
+++ StudyBoss/CardTableViewCell.swift
@@ -12,16 +12,21 @@ class CardTableViewCell: UITableViewCell {
     //MARK: Properties
     @IBOutlet weak var Front: UITextView!
     @IBOutlet weak var Back: UITextView!
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        //self.Front = Front
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
     }
 
 }
--- StudyBoss/FirstViewController.swift
+++ StudyBoss/FirstViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import os.log
 
-class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UINavigationControllerDelegate, UITextFieldDelegate {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UINavigationControllerDelegate, UITextFieldDelegate, UITableViewDataSourcePrefetching {
     
     //MARK: Properties
     
@@ -20,7 +20,11 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
     @IBOutlet weak var deckNameLabel: UILabel!
     @IBOutlet weak var saveButton: UIBarButtonItem!
     
-    
+    @IBOutlet weak var backTextField: UITextField!
+    @IBOutlet weak var frontTextField: UITextField!
+    weak open var prefetchDataSource: UITableViewDataSourcePrefetching?
+    //[self.customCollectionView setPrefetchingEnabled:NO]
+    var cells = [CardTableViewCell]()
     var deck: Deck?
     var cards = [Card]()
     var backs = [String]()// = ["Back of the Card2"]
@@ -32,21 +36,37 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     //MARK: Private Methods
     private func loadSampleCards(){
-        guard let card1 = Card(front: "Front loadSampleCards", back: "Back loadSampleCards") else{
+        let frontSample = "Front loadSampleCards"
+        let backSample = "Back loadSampleCards"
+        guard let card1 = Card(front: frontSample, back: backSample) else{
             fatalError("Unable to instantiate card1")
         }
+        guard let card2 = Card(front: frontSample, back: backSample) else{
+            fatalError("Unable to instantiate card2")
+        }
+        guard let card3 = Card(front: frontSample, back: backSample) else{
+            fatalError("Unable to instantiate card3")
+        }
+        guard let card4 = Card(front: frontSample, back: backSample) else{
+            fatalError("Unable to instantiate card3")
+        }
         
-        cards += [card1]
+        fronts += [frontSample, frontSample, frontSample, frontSample]
+        backs += [backSample, backSample, backSample, backSample]
+        cards += [card1, card2, card3, card4]
     }
     private func updateSaveButtonState() {
         // Disable the Save button if the text field is empty.
         //cards = tableView.cards
+        /*
         fronts = [String]()
         backs = [String]()
         for i in 0 ..< cards.count{
             fronts.append(cards[i].front)
             backs.append(cards[i].back)
         }
+        */
+        
         let text = nameTextField.text ?? ""
         saveButton.isEnabled = !text.isEmpty
     }
@@ -68,13 +88,34 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
     //MARK: Actions
     
     @IBAction func addData2(_ sender: Any) {
-        guard let newCard1 = Card(front: "Front AddCard", back: "Back AddCard") else{
+        /*
+        let frontAdd = "Front AddCard"
+        let backAdd = "Back AddCard"
+        guard let newCard1 = Card(front: frontAdd, back: backAdd) else{
             fatalError("Unable to instantiate newCard1")
         }
+        fronts += [frontAdd]
+        backs += [backAdd]
+        cards += [newCard1]
+        tableView.reloadData()
+ */
+        let frontAdd = frontTextField.text
+        let backAdd = backTextField.text
+        guard let newCard1 = Card(front: frontAdd!, back: backAdd!) else{
+            fatalError("Unable to instantiate newCard1")
+        }
+        fronts.append(frontAdd!)
+        backs.append(backAdd!)
         cards += [newCard1]
         tableView.reloadData()
     }
 
+    public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]){
+        
+    }
+    public func tableView(_tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]){
+        
+    }
     /*
     @IBAction func addData(sender: AnyObject){
         AddButton = sender as! UIButton
@@ -100,6 +141,37 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
  */
     //MARK: Navigation
+    /*
+    @IBAction func UpdateCards(_ sender: UIButton) {
+        updateCardView()
+    }
+ */
+    func updateCardView(){
+        /*
+         let index = IndexPath(row: 0, section: 0)
+         let cell: CardTableViewCell = self.tableView.cellForRow(at: index) as! CardTableViewCell
+         self.backs[0] = cell.Back.text
+         self.fronts[0] = cell.Front.text
+         */
+        fronts = [String]()
+        backs = [String]()
+        for i in 0 ..< cards.count{
+            fronts.append(cards[i].front)
+            backs.append(cards[i].back)
+        }
+        
+        //UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:<requiredIndexPath>]
+        for i in 0 ..< cards.count{
+            let index = IndexPath(row: i, section: 0)
+            let cell: CardTableViewCell = self.tableView.cellForRow(at: index) as! CardTableViewCell
+            self.backs[i] = cell.Back.text
+            self.fronts[i] = cell.Front.text
+            cards[i].back = backs[i]
+            cards[i].front = fronts[i]
+        }
+ 
+        tableView.reloadData()
+    }
     @IBAction func cancel(_ sender: UIBarButtonItem) {
         let isPresentingInAddDeckMode = presentingViewController is UINavigationController
         
@@ -147,7 +219,7 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
         // Do any additional setup after loading the view, typically from a nib.
         //nameTextField.delegate = self
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
-
+        tableView.prefetchDataSource = self
         nameTextField.delegate = self
         tableView.delegate = self
         tableView.dataSource = self as UITableViewDataSource
@@ -207,11 +279,54 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
         
     }
+    func tableview(_ tableView: UITableView, reloadRowsAt indexPath: IndexPath){
+        
+    }
+    /*
+    func collectionView(_ collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell{
+     
+    }
+ */
+    /*
+    func tableView(_ tableView: UITableView, didEndEditingRowAt indexPath: IndexPath?){
+     
+        for i in 0 ..< cards.count{
+            fronts.append(cards[i].front)
+            backs.append(cards[i].back)
+        }
+        for i in 0 ..< cards.count{
+            let index = IndexPath(row: i, section: 0)
+            let cell: CardTableViewCell = self.tableView.cellForRow(at: index) as! CardTableViewCell
+            self.backs[i] = cell.Back.text
+            self.fronts[i] = cell.Front.text
+            cards[i].back = backs[i]
+            cards[i].front = fronts[i]
+        }
+        let cell: CardTableViewCell = self.tableView.cellForRow(at: indexPath!) as! CardTableViewCell
+        let i =
+        self.backs[i] = cell.Back.text
+        self.fronts[i] = cell.Front.text
+        cards[i].back = backs[i]
+        cards[i].front = fronts[i]
+ 
+    }*/
+    //@IBOutlet weak var test: UITextView!
     func tableView(_ tableView: UITableView,
                             heightForRowAt indexPath: IndexPath) -> CGFloat{
          return 180
     }
-   
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            cards.remove(at: indexPath.row)
+            //saveCards()
+            //updateCardView()
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
 
 }
 
