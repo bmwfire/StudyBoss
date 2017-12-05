5774e2167c3f4d01883bf2316c059936c3153e2b
Andrew Millard <AKM@dhcp-168-105-252-64.wireless.manoa.hawaii.edu>
2017-12-05 10:02:45 -1000

    initial implementation of viewing cards

---
+++ StudyBoss/FlipViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FlipViewController.swift
+//  StudyBoss
+//
+//  Created by Andrew Millard on 12/5/17.
+//  Copyright © 2017 BMW Fire. All rights reserved.
+//
+
+import UIKit
+
+class FlipViewController: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
